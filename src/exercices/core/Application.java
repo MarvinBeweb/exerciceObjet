43213816d923f043e3f48a7870c13ec6ac173aee
Avitabile <marvin.avitabilebeweb@gmail.com>
2017-01-25 22:19:34 +0100

    exercice

---
+++ src/exercices/core/Application.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercices.core;
+
+/**
+ *
+ * @author marvin
+ */
+public  class Application implements ApplicationInterface {
+
+    @Override
+    public void start() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void create() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/exercices/heritage/animaux/entities/impl/Chien.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercices.heritage.animaux.entities.impl;
+
+/**
+ *
+ * @author marvin
+ */
+public  class Chien extends Animaux {
+      
+    public Chien() {
+        this.son = " aboie";
+    }
+    
+}  
+
